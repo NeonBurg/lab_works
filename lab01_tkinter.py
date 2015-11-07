2a43e8bdbd6d56f283bec7105ab52822ec3ef482
NeonBurg <gar-gary@yandex.ru>
2015-11-07 14:46:48 +0500

    Create lab01_tkinter.py
    Лабораторная 1
    Щавровского Николая
    РИС-13

---
+++ lab01_tkinter.py
@@ -0,0 +1,34 @@
+#Лабораторная 1
+#Щавровского Николая
+#РИС-13
+
+from Tkinter import *
+
+root = Tk()
+
+x = {
+                        "name": { "value": ""},
+                        "age": { "value": 0}
+                }
+
+def f():
+                for element in x.keys():
+                        print(element, x[element])
+
+for element in x.keys():
+                Label(root, text = element).pack()
+                #t = type(x[element]["value"])
+                t = x[element]["value"]
+                if type(t) is int:
+                        x[element]["var"] = IntVar()
+                elif isinstance(t, str):
+                        x[element]["var"] = StringVar()
+                else:
+                        raise TypeError
+
+                x[element]["entry"] = Entry(root, textvariable = x[element]["var"])
+                x[element]["entry"].pack()
+
+Button(root, text='Ok', command=f).pack()
+
+root.mainloop()
