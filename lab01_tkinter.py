0ee9136ca9b08aa2008d6c0315b25e0dcac7b600
NeonBurg <gar-gary@yandex.ru>
2015-11-09 08:51:39 +0500

    Лабораторная 1
    #Лабораторная 1
    #Щавровского Николая
    #РИС-13

--- lab01_tkinter.py
+++ lab01_tkinter.py
@@ -2,32 +2,31 @@
 #Щавровского Николая
 #РИС-13
 
-from Tkinter import *
+from tkinter import*
 
 root = Tk()
 
-x = {
-                        "name": { "value": ""},
-                        "age": { "value": 0}
-                }
+x = { 
+			"name": { "value": ""},
+			"age": { "value": 0}
+		}
 
 def f():
-                for element in x.keys():
-                        print(element, x[element])
+		for element in x.keys():
+			print(element, x[element])
 
 for element in x.keys():
-                Label(root, text = element).pack()
-                #t = type(x[element]["value"])
-                t = x[element]["value"]
-                if type(t) is int:
-                        x[element]["var"] = IntVar()
-                elif isinstance(t, str):
-                        x[element]["var"] = StringVar()
-                else:
-                        raise TypeError
+		Label(root, text = element).pack()
+		t = type(x[element]["value"])
+		if t == int:
+			x[element]["var"] = IntVar()
+		elseif t == str:
+			x[element]["var"] = StringVar()
+		else:
+			raise TypeError
 
-                x[element]["entry"] = Entry(root, textvariable = x[element]["var"])
-                x[element]["entry"].pack()
+		x[element]["entry"] = Entry(root, textvariable = x[element]["var"])
+		x[element]["entry"].pack()
 
 Button(root, text='Ok', command=f).pack()
 
